73ed992b3424a4afde33172bb320876294367d36
DarthNihilus1 <mgracer48@yahoo.com>
2023-11-05 15:10:01 -0500

    LightProjectile: Implementation

---
+++ Entities/Projectiles/LightProjectile.cs
@@ -0,0 +1,75 @@
+/*
+Light illuminating from the flashlight
+Indicates how far you can see
+Little recoil ; can't see for a second 
+Upgradeable object
+It stays alive for 5 seconds or dies hitting an object or an enemy 
+*/
+using System.Drawing;
+using System.Numerics;
+using AnotherLib.Collision;
+using FlashBOOM.Entities.Projectiles;
+
+namespace FlashBOOM.Entities.Projectiles
+{
+    public class LightProjectile : Projectile
+    {
+        public int timer = 0;
+        public int hitBoxWidth = 5;
+        public int hitBoxHeight = 5;
+        public float projectileSpeed = 1.0f; 
+        
+        public Vector2 velocity = new Vector2(0, 0);
+        
+        public LightProjectile()
+        {
+        }
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            Rectangle hitbox = new Rectangle((int)position.X, (int)position.Y, 
+                                              hitBoxWidth, hitBoxHeight);
+        }
+        /// <summary>
+        /// Moves the projectile by updating its position based on its velocity and the given speed.
+        /// </summary>
+        /// <param name="speed">The speed at which to move the projectile.</param>
+        private void Move(float speed)
+        {
+        
+            position.X += velocity.X * speed;
+            position.Y += velocity.Y * speed;
+
+        }
+        public override void Update()
+        {
+            base.Update();
+            timer++;
+            if (timer >= 5)
+            {
+                DestroyInstance();
+            }
+            if (DetectTileCollisionsByCollisionStyle(this.position))
+            {
+                DestroyInstance();
+            }
+            DetectCollisions(Main.activeEnemies)
+
+           
+            
+
+            
+        }
+        public override void HandleCollisions(CollisionBody collider, CollisionType colliderType)
+        {
+            base.HandleCollisions(collider, colliderType);
+            if (colliderType == CollisionType.Enemies)
+            {
+                DestroyInstance();
+            }
+        }
+    }
+    
+    
+}
